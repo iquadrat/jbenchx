27d1232d7949406df1e97409c3a3b3299184c216
Micha Riser <micha@povworld.org>
2011-08-07 01:40:16 +0200

    initial version

---
+++ org.jbench.test/src/org/jbench/test/BenchmarkRunnterTest.java
@@ -0,0 +1,19 @@
+package org.jbench.test;
+
+import org.jbench.*;
+import org.junit.*;
+
+
+
+public class BenchmarkRunnterTest extends BenchmarkTestCase {
+  
+  @Test
+  public void testRun_ArrayListBenchmark() {
+    
+    BenchmarkRunner runner = new BenchmarkRunner();
+    runner.add(ArrayListBenchmark.class);
+    runner.run(IProgressMonitor.DUMMY);
+    
+  }
+  
+}
