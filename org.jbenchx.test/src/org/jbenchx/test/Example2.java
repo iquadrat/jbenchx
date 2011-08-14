b6e63cce0169865a08d98591d26ba2076fea4b2f
Micha Riser <micha@povworld.org>
2011-08-14 22:31:35 +0200

    add wizard-made project for eclipse integration

---
+++ org.jbenchx.test/src/org/jbenchx/test/Example2.java
@@ -0,0 +1,55 @@
+package org.jbenchx.test;
+
+import java.security.*;
+
+import org.jbenchx.*;
+import org.jbenchx.annotations.*;
+import org.jbenchx.monitor.*;
+
+import edu.umd.cs.findbugs.annotations.*;
+
+public class Example2 extends Benchmark {
+  
+  private int   sval;
+  private int[] vals;
+  
+  public Example2() throws Exception {
+    sval = 0;
+    vals = new int[1000];
+    for (int i = 0; i < 1000; ++i) {
+      vals[i] = i;
+    }
+  }
+  
+  @Bench
+  public void test_assert() {
+    for (int i = 0; i < 1000; ++i) {
+      int val = vals[i];
+      assert (val >= 0): "should be positive";
+      sval = val * 6;
+      sval += 3; // Read/write global
+      sval /= 2;
+    }
+  }
+  
+  @Bench
+  public void test_assert2() {
+    for (int i = 0; i < 1000; ++i) {
+      int val = vals[i];
+      if (val < 0) throw new IllegalArgumentException();
+      sval = val * 6;
+      sval += 3; // Read/write global
+      sval /= 2;
+    }
+  }
+  
+  public static void main(String[] args) throws Exception {
+    
+    BenchmarkRunner runner = new BenchmarkRunner();
+    runner.add(Example2.class);
+    
+    runner.run(BenchmarkContext.create(new ConsoleProgressMonitor()));
+    
+  }
+  
+}
