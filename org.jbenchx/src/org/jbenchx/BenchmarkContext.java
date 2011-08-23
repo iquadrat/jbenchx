d3f66f4a39d221eb9cf51426507efeff5fbfc899
Micha Riser <micha@povworld.org>
2011-08-23 22:17:12 +0200

    remove debug sysout

--- org.jbenchx/src/org/jbenchx/BenchmarkContext.java
+++ org.jbenchx/src/org/jbenchx/BenchmarkContext.java
@@ -10,8 +10,11 @@ public class BenchmarkContext {
   private final VmInfo              fVmInfo = VmInfo.getVmInfo();
   
   private final IProgressMonitor    fProgressMonitor;
+  
   private final long                fTimerGranularity;
+  
   private final long                fMethodInvokeTime;
+  
   private final BenchmarkParameters fDefaultParams;
   
   public BenchmarkContext(IProgressMonitor progressMonitor, long timerGranularity, long methodInvokeTime) {
@@ -52,8 +55,8 @@ public class BenchmarkContext {
     runner.add(SystemBenchmark.class);
     BenchmarkResult result = runner.run(systemBenchmarkContext);
     
-    BenchmarkTask sqrtTask = result.findTask(SystemBenchmark.class.getSimpleName()+".sqrt");
-    BenchmarkTask sqrt1Task = result.findTask(SystemBenchmark.class.getSimpleName()+".sqrt1");
+    BenchmarkTask sqrtTask = result.findTask(SystemBenchmark.class.getSimpleName() + ".sqrt");
+    BenchmarkTask sqrt1Task = result.findTask(SystemBenchmark.class.getSimpleName() + ".sqrt1");
     if (sqrtTask == null || sqrt1Task == null) {
       throw new RuntimeException("Failed to run SystemBenchmark");
     }
@@ -61,7 +64,7 @@ public class BenchmarkContext {
     TaskResult sqrtResult = result.getResult(sqrt1Task);
     TaskResult sqrt1Result = result.getResult(sqrtTask);
     
-    System.out.println("sqrt="+sqrtResult.getEstimatedBenchmark()+" sqrt1="+sqrt1Result.getEstimatedBenchmark());
+//    System.out.println("sqrt="+sqrtResult.getEstimatedBenchmark()+" sqrt1="+sqrt1Result.getEstimatedBenchmark());
     long methodInvokeTime = Math.round(sqrtResult.getEstimatedBenchmark() - sqrt1Result.getEstimatedBenchmark());
     
 //    long methodInvokeTime = -1;
@@ -73,7 +76,7 @@ public class BenchmarkContext {
     
     long timerGranularity = TimeUtil.estimateTimerGranularity(new Timer());
     
-    System.out.println("Timer Granularity: " + TimeUtil.toString(timerGranularity) + ", invoke: " + TimeUtil.toString(methodInvokeTime));
+//    System.out.println("Timer Granularity: " + TimeUtil.toString(timerGranularity) + ", invoke: " + TimeUtil.toString(methodInvokeTime));
 //      BenchmarkContext systemContext = new BenchmarkContext(new ConsoleProgressMonitor());
 //      BenchmarkTask task = new BenchmarkTask(SystemBenchmark.class.getSimpleName(), SystemBenchmark.class.getCanonicalName(), "estimateTimerGranularity", 
 //          new BenchmarkParameters(0, 1, 5, 50, 5, 0.1));
