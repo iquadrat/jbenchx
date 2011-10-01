ec7be488e6c0ade76186b9413ae39e2bc9bd389c
Micha Riser <micha@povworld.org>
2011-10-01 16:34:58 +0200

    Tester no longer needs to check for subclassing Benchmark

--- org.jbenchx.ui.eclipse/src/org/jbenchx/ui/eclipse/JBenchXPropertyTester.java
+++ org.jbenchx.ui.eclipse/src/org/jbenchx/ui/eclipse/JBenchXPropertyTester.java
@@ -4,26 +4,27 @@ import org.eclipse.core.expressions.*;
 import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.*;
 import org.eclipse.jdt.core.*;
-import org.jbenchx.*;
 import org.jbenchx.annotations.*;
 
 public class JBenchXPropertyTester extends PropertyTester {
-  
+
   public JBenchXPropertyTester() {}
-  
+
   @Override
   public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
     IAdaptable adaptable = (IAdaptable)receiver;
     IResource resource = (IResource)adaptable.getAdapter(IResource.class);
-    if (resource == null) return false;
-    
+    if (resource == null) {
+      return false;
+    }
+
     if ("containsBench".equals(property)) {
       return containsBench(resource);
     }
-    
+
     return false;
   }
-  
+
   private boolean containsBench(IResource resource) {
     IJavaElement javaElement = JavaCore.create(resource);
     if (javaElement instanceof ITypeRoot) {
@@ -31,39 +32,28 @@ public class JBenchXPropertyTester extends PropertyTester {
     }
     return false;
   }
-  
+
   private boolean containsBench(ITypeRoot typeRoot) {
     IType primaryType = typeRoot.findPrimaryType();
-    if (primaryType == null || !isSubclassOfBenchmark(primaryType)) return false;
-    
+    if (primaryType == null) {
+      return false;
+    }
+
     try {
       for (IMethod method: primaryType.getMethods()) {
-        
+
         for (IAnnotation annotation: method.getAnnotations()) {
           String name = annotation.getElementName();
           if (Bench.class.getSimpleName().equals(name) || Bench.class.getName().equals(name)) {
             return true;
           }
         }
-        
+
       }
     } catch (JavaModelException e) {
     }
-    
+
     return false;
   }
-  
-  private boolean isSubclassOfBenchmark(IType primaryType) {
-    try {
-      IRegion region = JavaCore.newRegion();
-      region.add(primaryType);
-      ITypeHierarchy typeHierarchy = JavaCore.newTypeHierarchy(region, null, null);
-      for (IType type: typeHierarchy.getSupertypes(primaryType)) {
-        if (Benchmark.class.getName().equals(type.getFullyQualifiedName())) return true;
-      }
-    } catch (JavaModelException e) {
-    }
-    return false;
-  }
-  
+
 }
